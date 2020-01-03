d904334976b927d5db2372ee5199d414a8c6257d
mnsmanasa <saimanasa1502@gmail.com>
2020-01-03 19:50:09 +0530

    1 file modified, 1 added

---
+++ Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return <div>board</div>
+  }
+}
+
+export default Board
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Board from './Board';
 import './style.css';
 
 class App extends Component {
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        hi
+        hi <Board />
       </div>
     );
   }
