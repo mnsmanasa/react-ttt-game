2484f996697e3fb49f37cfeeceeccdff0012a5c4
mnsmanasa <saimanasa1502@gmail.com>
2020-01-06 16:23:29 +0530

    1 file modified

--- Square.js
+++ Square.js
@@ -3,27 +3,13 @@ import React from "react";
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      squareValue: "",
-      xIsNext: true
-    };
+    this.state = {};
   }
-
-  handleSquare = id => {
-    const squares = this.state.squares.slice();
-    squares[id] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      xIsNext: !this.state.xIsNext,
-      squares: squares,
-      squareValue: this.state.xIsNext ? "X" : "O"
-    });
-  };
 
   render() {
     return (
-      <div className="square" onClick={() => this.handleSquare(this.props.id)}>
-        {this.state.squareValue}
+      <div className="square" onClick={props.onClick}>
+        {props.value}
       </div>
     );
   }
