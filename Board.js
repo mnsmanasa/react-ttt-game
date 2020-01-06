aa03d2976219dfab69d74435a96b45d30b01a7dd
mnsmanasa <saimanasa1502@gmail.com>
2020-01-06 15:56:30 +0530

    1 file modified

--- Board.js
+++ Board.js
@@ -15,7 +15,17 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       squareValue: "",
-      isNextPlayer: true
+      isNextPlayer: true,
+      winnerPossibilities: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [1,4,8],
+        [2,4,6]        
+      ]
     };
   }
 
